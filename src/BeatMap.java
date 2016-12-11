2fd3470007c66762924879675cc653c4412b88c2
Yuting Liu <yuting9@gmail.com>
2016-12-10 20:58:24 -0500

    Fixed up ActionListener on MainMenu

--- src/BeatMap.java
+++ src/BeatMap.java
@@ -1,6 +1,17 @@
 
 public class BeatMap {
 	private static int vars; // Placeholder
+	private static Note[] map;
 	
 	
+}
+
+class Note{
+	int length, position, time;
+	
+	Note(int p, int l, int t){
+		position = p;
+		length = l;
+		time = t;
+	}
 }
--- src/Instructions.java
+++ src/Instructions.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 public class Instructions extends JPanel{
 	private static final long serialVersionUID = 1L;
 	
+	
 	public Instructions(){
 		System.out.println("In instructions mode");
 	}
