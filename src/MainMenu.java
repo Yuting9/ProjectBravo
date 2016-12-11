a6892b6d56105a97dd4f4092b1b48da2f38c5868
Yuting Liu <yuting9@gmail.com>
2016-12-10 21:13:32 -0500

    Make MainMenu Great (Again)

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,61 +1,46 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-public class MainMenu extends JPanel
-{	
+public class MainMenu extends JPanel implements ActionListener{	
 	private static final long serialVersionUID = 1L;
 	JButton btnArray[] = new JButton[4];
+	JPanel btnPanel = new JPanel();
 	
-	public MainMenu()
-	{
-		this.setLayout(null);
-		ButtonListener btnListener = new ButtonListener();
+	public MainMenu(){
+		this.setLayout(new BorderLayout());
 		btnArray[0] = new JButton("How to Play");
 		btnArray[1] = new JButton("Player Mode");
 		btnArray[2] = new JButton("Creator Mode");
 		btnArray[3] = new JButton("EXIT");
-		for(int i=0; i<4; i++)
-		{
+		btnPanel.setLayout(null);
+		this.add(btnPanel, BorderLayout.CENTER);
+		for(int i=0; i<4; i++){
 			btnArray[i].setLocation(150, 300+(i*55));
-			btnArray[i].setSize(100, 50);
-			btnArray[i].addActionListener(btnListener);
-			this.add(btnArray[i]);
+			btnArray[i].setSize(150, 50);
+			btnArray[i].addActionListener(this);
+			btnPanel.add(btnArray[i]);
 		}
 	}
 	
-	private class ButtonListener implements ActionListener
-	{
-
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			// TODO Auto-generated method stub
-			Main.frame.repaint();
-			if(arg0.getSource() == btnArray[0])
-			{
-				GameFrame.clear();
-				GameFrame.state="Instructions";
-				GameFrame.add(new Instructions());
-			}
-			else if(arg0.getSource() == btnArray[1])
-			{
-				GameFrame.clear();
-				GameFrame.state="Game";
-				System.out.println("magic");
-				Game.GameSetup();
-			}
-			else if(arg0.getSource() == btnArray[2])
-			{
-				GameFrame.clear();
-				GameFrame.state="Edit";
-				GameFrame.add(new Edit());
-			}
-			else if(arg0.getSource() == btnArray[3])
-			{
-				Main.frame.dispose();
-			}
+	public void actionPerformed(ActionEvent arg0) {
+		if(arg0.getSource() == btnArray[0]){
+			GameFrame.clear();
+			GameFrame.state="Instructions";
+			GameFrame.add(new Instructions());
 		}
-		
+		else if(arg0.getSource() == btnArray[1]){
+			GameFrame.clear();
+			GameFrame.state="Game";
+			System.out.println("magic");
+			Game.GameSetup();
+		}
+		else if(arg0.getSource() == btnArray[2]){
+			GameFrame.clear();
+			GameFrame.state="Edit";
+			GameFrame.add(new Edit());
+		}
+		else if(arg0.getSource() == btnArray[3]){
+			Main.frame.dispose();
+		}
 	}
-	
-	
 }
