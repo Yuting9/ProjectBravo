5b20c399efa7e5aa965aa818332de8afbb734246
Yuting Liu <yuting9@gmail.com>
2017-01-03 21:48:54 -0500

    Further Changes

--- src/Setup.java
+++ src/Setup.java
@@ -2,6 +2,7 @@ import java.awt.event.*;
 import java.io.*;
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Setup extends JPanel implements ActionListener{
 	
@@ -9,7 +10,11 @@ public class Setup extends JPanel implements ActionListener{
 	private JPanel pnlChoices;
 	private JPanel pnlChoSong;
 	private JPanel pnlPicture;
+	private JPanel pnlFileNom;
+	private JPanel pnlGetSong;
 	private JPanel pnlPlaSong;
+	private JPanel pnlSongBtn;
+	private JPanel pnlSongLen;
 	private JPanel pnlControl;
 	
 	private JButton btnConfirm;
@@ -39,6 +44,10 @@ public class Setup extends JPanel implements ActionListener{
 		pnlChoices = new JPanel();
 		pnlChoSong = new JPanel();
 		pnlPicture = new JPanel();
+		pnlFileNom = new JPanel();
+		pnlSongLen = new JPanel();
+		pnlSongBtn = new JPanel();
+		pnlGetSong = new JPanel();
 		pnlPlaSong = new JPanel();
 		pnlControl = new JPanel();
 		
@@ -49,6 +58,14 @@ public class Setup extends JPanel implements ActionListener{
 		btnPlay    = new JButton("Play");
 		btnStop    = new JButton("Stop");
 		btnPause   = new JButton("Pause");
+		
+		btnConfirm.addActionListener(this);
+		btnCancel .addActionListener(this);
+		btnChoSong.addActionListener(this);
+		btnChoPic .addActionListener(this);
+		btnPlay   .addActionListener(this);
+		btnStop   .addActionListener(this);
+		btnPause  .addActionListener(this);
 
 		Audio sampleAud = new Audio();
 		
@@ -60,8 +77,15 @@ public class Setup extends JPanel implements ActionListener{
 		
 		//----------------------Setting Parameters------------------------------
 		
+		pnlConfirm.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		pnlChoices.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		pnlChoSong.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		pnlPicture.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		pnlFileNom.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		pnlSongLen.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		pnlGetSong.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		pnlPlaSong.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		pnlControl.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		
 		songProgress.setMaximum((int)sampleAud.getLength());
 		songProgress.setSize((this.getWidth()/4)-10, 10);
@@ -71,27 +95,40 @@ public class Setup extends JPanel implements ActionListener{
 		pnlConfirm.setLayout(new FlowLayout());
 		pnlChoSong.setLayout(new GridLayout(0, 1, 0, 0));
 		pnlPlaSong.setLayout(new GridLayout(0, 1, 0, 0));
+		pnlGetSong.setLayout(new GridLayout(0, 1, 0, 0));
+		pnlSongLen.setLayout(new BorderLayout());
+		
+		//--------------------Adding Components----------------------------------
 
+		add(pnlConfirm, BorderLayout.SOUTH);
+		add(pnlChoices, BorderLayout.CENTER);
+		
 		pnlConfirm.add(btnCancel);
 		pnlConfirm.add(btnConfirm);
-		add(pnlConfirm, BorderLayout.SOUTH);
-
 		pnlChoices.add(pnlChoSong);
-		pnlChoSong.add(lblSong);
-		pnlChoSong.add(btnChoSong);
-		pnlChoSong.add(lblInfo);
+		pnlChoSong.add(pnlGetSong);
+		pnlGetSong.add(pnlFileNom);
+		pnlFileNom.add(lblSong);
+		pnlGetSong.add(pnlSongBtn);
+		pnlSongBtn.add(btnChoSong);
 		pnlChoSong.add(pnlPlaSong);
-		pnlPlaSong.add(songProgress);
+		pnlPlaSong.add(pnlSongLen);
+		pnlSongLen.add(songProgress, BorderLayout.SOUTH);
 		pnlPlaSong.add(pnlControl);
 		pnlControl.add(btnPlay);
 		pnlControl.add(btnPause);
 		pnlControl.add(btnStop);
+		pnlGetSong.add(lblInfo);
+		
+		
+		
+		
+		
 		
 		pnlChoices.add(pnlPicture);
-		add(pnlChoices, BorderLayout.CENTER);
 		
 		GameFrame.add(this);
-
+		
 		this.setVisible(true);
 		this.revalidate();
 		this.repaint();
@@ -101,6 +138,45 @@ public class Setup extends JPanel implements ActionListener{
 	public void actionPerformed(ActionEvent arg0) {
 		if(arg0.getSource() == btnCancel){
 			
+		}
+		if(arg0.getSource() == btnConfirm){
+			/*
+			if(s != null){
+				GameFrame.clear();
+				GameFrame.state="Edit";
+				new Edit(choose.getSelectedFile().getAbsoluteFile(),s);
+			}
+			*/
+		}
+		if(arg0.getSource() == btnChoSong){
+
+			JOptionPane.showMessageDialog(this, "Please Choose a song (Must be in WAV format)");
+			FileNameExtensionFilter filter = new FileNameExtensionFilter("WAV File", "wav");
+			JFileChooser choose = new JFileChooser();
+			choose.setVisible(true);
+			choose.setFileFilter(filter);
+			choose.setAcceptAllFileFilterUsed(false);
+			int result = choose.showOpenDialog(this);
+			while((!choose.getSelectedFile().getName().substring(
+					choose.getSelectedFile().getName().lastIndexOf(".")+1,
+					choose.getSelectedFile().getName().length()).equals("wav") || 
+					choose.getSelectedFile() == null) && 
+					result == JFileChooser.APPROVE_OPTION){
+				result = choose.showOpenDialog(this);
+			}
+			if(choose.getSelectedFile() != null && result == JFileChooser.APPROVE_OPTION){
+				String s = null;
+				do{
+					if(s != null){
+						JOptionPane.showMessageDialog(this, "Invalid Choice - Song already exists");
+					}
+					s = (String) JOptionPane.showInputDialog(this, "Enter the song's name:\n","Enter Name", JOptionPane.PLAIN_MESSAGE);	
+					if(s == null){
+						break;
+					}
+				}while(!(new File("/src/Songs/"+s).mkdirs()));
+				new File("/src/Songs/"+s).delete();
+			}
 		}
 	}
 
--- src/TEST_REMOVE_FROM_FINAL.java
+++ src/TEST_REMOVE_FROM_FINAL.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.Component;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.FlowLayout;
 
 public class TEST_REMOVE_FROM_FINAL {
 	static JFrame mainFrame = new JFrame();
@@ -9,28 +10,17 @@ public class TEST_REMOVE_FROM_FINAL {
 		mainFrame.getContentPane().setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel = new JPanel();
-		mainFrame.getContentPane().add(panel);
-		panel.setLayout(new GridLayout(0, 1, 0, 0));
+		mainFrame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
 		JButton btnNewButton = new JButton("New button");
 		panel.add(btnNewButton);
-		
-		JLabel label = new JLabel("");
-		panel.add(label);
 		
 		JButton btnNewButton_1 = new JButton("New button");
 		panel.add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("New button");
 		panel.add(btnNewButton_2);
-		
-		JPanel panel_1 = new JPanel();
-		panel.add(panel_1);
-		
-		JProgressBar progressBar = new JProgressBar();
-		progressBar.setValue(4135);
-		progressBar.setMaximum(1351325);
-		panel.add(progressBar);
 		mainFrame.setVisible(true);
 	}
 
